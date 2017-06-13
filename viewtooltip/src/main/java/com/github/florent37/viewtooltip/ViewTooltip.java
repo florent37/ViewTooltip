875c57edb64ee8483a1ceb52901574cb02f8b27f
florent champigny <florent.champigny@backelite.com>
2017-06-13 09:15:54 +0200

    v1.0.1

--- viewtooltip/src/main/java/com/github/florent37/viewtooltip/ViewTooltip.java
+++ viewtooltip/src/main/java/com/github/florent37/viewtooltip/ViewTooltip.java
@@ -9,6 +9,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
@@ -465,6 +466,28 @@ public class ViewTooltip {
                 }
             }
         }
+    }
+
+    private static Path drawBubble(RectF rect, float topLeftDiameter, float topRightDiameter, float bottomRightDiameter, float bottomLeftDiameter){
+        Path path = new Path();
+
+        topLeftDiameter = topLeftDiameter < 0 ? 0 : topLeftDiameter;
+        topRightDiameter = topRightDiameter < 0 ? 0 : topRightDiameter;
+        bottomLeftDiameter = bottomLeftDiameter < 0 ? 0 : bottomLeftDiameter;
+        bottomRightDiameter = bottomRightDiameter < 0 ? 0 : bottomRightDiameter;
+
+        path.moveTo(rect.left + topLeftDiameter/2 ,rect.top);
+        path.lineTo(rect.right - topRightDiameter/2,rect.top);
+        path.quadTo(rect.right, rect.top, rect.right, rect.top + topRightDiameter/2);
+        path.lineTo(rect.right ,rect.bottom - bottomRightDiameter/2);
+        path.quadTo(rect.right ,rect.bottom, rect.right - bottomRightDiameter/2, rect.bottom);
+        path.lineTo(rect.left + bottomLeftDiameter/2,rect.bottom);
+        path.quadTo(rect.left,rect.bottom,rect.left, rect.bottom - bottomLeftDiameter/2);
+        path.lineTo(rect.left,rect.top + topLeftDiameter/2);
+        path.quadTo(rect.left,rect.top, rect.left + topLeftDiameter/2, rect.top);
+        path.close();
+
+        return path;
     }
 
 }
