194c80ff77d1712a30a6cbca412a0f3ca3c261b7
florent champigny <florent.champigny@backelite.com>
2018-09-13 10:31:36 +0200

    v1.1.7

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . ~/.bash_profile
-./gradlew clean assembleDebug install
-./gradlew bintrayUpload
+./gradlew clean
+./gradlew :viewtooltip:assembleDebug :viewtooltip:install
+./gradlew :viewtooltip:bintrayUpload
